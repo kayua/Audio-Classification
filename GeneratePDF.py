d913c0080554caa76f7156bf755727c2fbbed3e8
kayua <kayuaolequesp@gmail.com>
2024-07-25 20:36:08 -0300

    Create

--- GeneratePDF.py
+++ GeneratePDF.py
@@ -2,55 +2,69 @@ import markdown
 import pdfkit
 import os
 
-
 def convert_md_to_html(md_file_path, html_file_path):
-    # Lê o arquivo Markdown
+    # Read the markdown file
     with open(md_file_path, 'r', encoding='utf-8') as md_file:
         md_content = md_file.read()
 
-    # Converte Markdown para HTML
+    # Convert markdown to HTML
     html_content = markdown.markdown(md_content, extensions=['tables', 'fenced_code'])
 
-    # Escreve o HTML em um arquivo
+    # Add CSS for image centering
+    html_content = f"""
+    <html>
+    <head>
+        <style>
+            img {{
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                max-width: 100%;
+            }}
+        </style>
+    </head>
+    <body>
+        {html_content}
+    </body>
+    </html>
+    """
+
+    # Write HTML content to a file
     with open(html_file_path, 'w', encoding='utf-8') as html_file:
         html_file.write(html_content)
 
-
 def convert_html_to_pdf(html_file_path, pdf_file_path):
-    # Configure pdfkit para usar wkhtmltopdf
-    path_wkhtmltopdf = '/usr/bin/wkhtmltopdf'  # Atualize este caminho conforme necessário
+    # Path to wkhtmltopdf
+    path_wkhtmltopdf = '/usr/bin/wkhtmltopdf'
     config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
 
-    # Defina o diretório base para recursos locais
+    # Options to set margins and enable local file access
     options = {
         'no-outline': None,
-        'enable-local-file-access': None,  # Habilita o acesso a arquivos locais
+        'enable-local-file-access': None,
+        'margin-top': '20mm',  # Margin superior
+        'margin-bottom': '20mm'  # Margin inferior
     }
 
-    # Converte HTML para PDF
     try:
+        # Convert HTML to PDF
         pdfkit.from_file(html_file_path, pdf_file_path, configuration=config, options=options)
     except Exception as e:
         print(f'Error converting HTML to PDF: {e}')
 
-
 if __name__ == '__main__':
-    # Caminho para o arquivo Markdown
+    # Paths for input and output files
     md_file_path = 'ReadMe.md'
-
-    # Caminho para o arquivo HTML temporário
     html_file_path = 'temp.html'
-
-    # Caminho para o arquivo PDF
     pdf_file_path = 'output.pdf'
 
-    # Converta Markdown para HTML
+    # Convert markdown to HTML
     convert_md_to_html(md_file_path, html_file_path)
 
-    # Converta HTML para PDF
+    # Convert HTML to PDF
     convert_html_to_pdf(html_file_path, pdf_file_path)
 
-    # Remove o arquivo HTML temporário
+    # Remove temporary HTML file
     os.remove(html_file_path)
 
     print(f'Success: The PDF has been generated at {pdf_file_path}.')
