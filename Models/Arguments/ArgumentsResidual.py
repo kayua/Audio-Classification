80e44accb73c52c1b496ff14f5c47432cdb51bfc
kayua <kayuaolequesp@gmail.com>
2025-03-03 15:24:10 -0300

    Improvement documentation

---
+++ Models/Arguments/ArgumentsResidual.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'unknown'
+__email__ = 'unknown@unknown.com.br'
+__version__ = '{1}.{0}.{0}'
+__initial_data__ = '2024/07/17'
+__last_update__ = '2024/07/17'
+__credits__ = ['unknown']
+
+
+try:
+    import os
+    import sys
+    import glob
+
+except ImportError as error:
+
+    print(error)
+    print("1. Install requirements:")
+    print("  pip3 install --upgrade pip")
+    print("  pip3 install -r requirements.txt ")
+    print()
+    sys.exit(-1)
+
+# Default values
+DEFAULT_SAMPLE_RATE = 8000
+DEFAULT_HOP_LENGTH = 256
+DEFAULT_SIZE_BATCH = 32
+DEFAULT_WINDOW_SIZE_FACTOR = 40
+DEFAULT_NUMBER_FILTERS_SPECTROGRAM = 512
+DEFAULT_FILTERS_PER_BLOCK = [16, 32, 64, 96]
+DEFAULT_FILE_EXTENSION = "*.wav"
+DEFAULT_DROPOUT_RATE = 0.1
+DEFAULT_NUMBER_LAYERS = 4
+DEFAULT_OPTIMIZER_FUNCTION = 'adam'
+DEFAULT_OVERLAP = 2
+DEFAULT_LOSS_FUNCTION = 'sparse_categorical_crossentropy'
+DEFAULT_DECIBEL_SCALE_FACTOR = 80
+DEFAULT_CONVOLUTIONAL_PADDING = 'same'
+DEFAULT_INPUT_DIMENSION = (513, 40, 1)
+DEFAULT_INTERMEDIARY_ACTIVATION = 'relu'
+DEFAULT_LAST_LAYER_ACTIVATION = 'softmax'
+DEFAULT_NUMBER_CLASSES = 4
+DEFAULT_SIZE_POOLING = (2, 2)
+DEFAULT_WINDOW_SIZE = 1024
+DEFAULT_NUMBER_EPOCHS = 10
+DEFAULT_NUMBER_SPLITS = 5
+DEFAULT_SIZE_CONVOLUTIONAL_FILTERS = (3, 3)
+
+
+def get_residual_model_args(parser):
+
+    parser.add_argument('--residual_hop_length', type=int,
+                        default=DEFAULT_HOP_LENGTH, help='Hop length for STFT')
+
+    parser.add_argument('--residual_window_size_factor', type=int,
+                        default=DEFAULT_WINDOW_SIZE_FACTOR, help='Factor applied to FFT window size')
+
+    parser.add_argument('--residual_number_filters_spectrogram', type=int,
+                        default=DEFAULT_NUMBER_FILTERS_SPECTROGRAM, help='Number of filters for spectrogram generation')
+
+    parser.add_argument('--residual_filters_per_block',
+                        default=DEFAULT_FILTERS_PER_BLOCK, help='Number of filters in each convolutional block')
+
+    parser.add_argument('--residual_dropout_rate', type=float,
+                        default=DEFAULT_DROPOUT_RATE, help='Dropout rate in the network')
+
+    parser.add_argument('--residual_number_layers', type=int,
+                        default=DEFAULT_NUMBER_LAYERS, help='Number of convolutional layers')
+
+    parser.add_argument('--residual_overlap', type=int,
+                        default=DEFAULT_OVERLAP, help='Overlap between patches in the spectrogram')
+
+    parser.add_argument('--residual_decibel_scale_factor', type=float,
+                        default=DEFAULT_DECIBEL_SCALE_FACTOR, help='Scale factor for converting to decibels')
+
+    parser.add_argument('--residual_convolutional_padding', type=str,
+                        default=DEFAULT_CONVOLUTIONAL_PADDING, help='Padding type for convolutional layers')
+
+    parser.add_argument('--residual_intermediary_activation', type=str,
+                        default=DEFAULT_INTERMEDIARY_ACTIVATION, help='Activation function for intermediary layers')
+
+    parser.add_argument('--residual_last_layer_activation', type=str,
+                        default=DEFAULT_LAST_LAYER_ACTIVATION, help='Activation function for the last layer')
+
+    parser.add_argument('--residual_size_pooling', type=tuple,
+                        default=DEFAULT_SIZE_POOLING, help='Size of the pooling layers')
+
+    parser.add_argument('--residual_window_size', type=int,
+                        default=DEFAULT_WINDOW_SIZE, help='Size of the FFT window')
+
+    parser.add_argument('--residual_size_convolutional_filters', type=tuple,
+                        default=DEFAULT_SIZE_CONVOLUTIONAL_FILTERS, help='Size of the convolutional filters')
+
+    return parser
